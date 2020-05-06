eb90d16a3ce2112a28a19961ec32370e24277aa9
Trezentos <gustavofagundes1998hotmail.com>
2020-05-06 17:48:34 -0300

    Multiplica, divisao e sistema de historico adicionados!

--- app/src/main/java/com/example/calculadora/MainActivity.java
+++ app/src/main/java/com/example/calculadora/MainActivity.java
@@ -12,6 +12,8 @@ public class MainActivity extends AppCompatActivity {
     EditText numero1;
     EditText numero2;
     TextView resultado;
+    TextView historico;
+    int histContador = 0;
 
 
     @Override
@@ -19,10 +21,43 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         numero1 = findViewById(R.id.etNum1);
         numero2 = findViewById(R.id.etNum2);
         resultado = findViewById(R.id.Visao);
+        historico = findViewById(R.id.historico);
+
     }
+
+    public void limparHistorico(View v){
+
+        historico.setText("Histórico");
+        histContador=0;
+
+    }
+
+    public void historicoSistema(TextView historico, float res){
+
+        String limpar = historico.toString();
+
+        if(limpar == "Limpa isso"){
+            histContador=0;
+            historico.setText("Histórico\n");
+            return;
+        }
+
+
+        if( histContador < 5 ){
+            historico.append("\n"+res);
+            histContador++;
+        }else{
+            histContador=0;
+            historico.setText("Histórico\n"+res);
+        }
+
+    }
+
+
 
     public void adicaoResultado (View v ){
         //Trimm ele limpa os espaços
@@ -35,6 +70,10 @@ public class MainActivity extends AppCompatActivity {
         }else{
             float res = Float.valueOf(num1St) + Float.valueOf(num2St);
             resultado.setText("Resultado: " + res);
+
+            historicoSistema(historico, res);
+
+
         }
     }
 
@@ -47,8 +86,39 @@ public class MainActivity extends AppCompatActivity {
         }else{
             float res = Float.valueOf(num1St) - Float.valueOf(num2St);
             resultado.setText("Resultado: " + res);
+            historicoSistema(historico, res);
         }
 
     }
+
+    public void multiplicacaoResultado(View view){
+        String num1St = numero1.getText().toString().trim();
+        String num2St = numero2.getText().toString().trim();
+
+        if(num1St.equals("") || num2St.equals("") ){
+            Toast.makeText(this,"Por favor informar os dois numeros", Toast.LENGTH_SHORT).show();
+        }else{
+            float res = Float.valueOf(num1St)*Float.valueOf(num2St);
+            resultado.setText("Resultado: "+res);
+            historicoSistema(historico, res);
+        }
+
+    }
+
+    public void divisaoResultado(View view){
+        String num1St = numero1.getText().toString().trim();
+        String num2St = numero2.getText().toString().trim();
+
+        if(num1St.equals("") || num2St.equals("")){
+            Toast.makeText(this, "Por favor informar os dois numero", Toast.LENGTH_SHORT);
+        }else{
+            float res = Float.valueOf(num1St)/Float.valueOf(num2St);
+            resultado.setText("Resultado "+res);
+            historicoSistema(historico, res);
+        }
+    }
+
+
+
 
 }
